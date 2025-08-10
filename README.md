af71b72e1f7e3d9413686751190bcfe6ddbb1c85
Your Name <neeraj@Neerajs-MacBook-Air.local>
2025-08-11 01:12:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# ArtVendor
+
+A MERN stack application designed to buy art they need.
+
+- frontend live link: https://artvendor.onrender.com/
+
+- admin portal live link:- https://artvendor-admin.onrender.com/
+
+# Description
+
+ArtVendor is a one-stop solution for purchasing your favorite crafts. The platform includes the following pages:
+
+_1.Home Page:_
+
+- Product Selection: Users can browse and select their favorite crafts.
+
+- Quantity Selection: Users can specify the quantity for each product.
+
+_2.Cart Page:_
+
+- Review Items: Users can review the items they have added to their cart.
+
+_3.Payment Checkout Page:_
+
+- Payment Processing: Users can enter payment details and complete the purchase.
+
+_4.Orders Page:_
+
+Order Tracking: Users can keep track of their past orders..
+
+_5. Admin Panel:_
+
+- For admin to add/remove/update status
+
+# Install Dependencies
+
+> cd frontend and npm i
+>
+> cd admin and npm i
+>
+> cd backend and npm i
+>
+> in backend add .env file and add JWT_SECRET , MONGODB_URL , PORT and STRIPE_SECRET_KEY
+
+# Images:
+![Screenshot 2024-07-19 183625](https://github.com/user-attachments/assets/2958e39b-ddd7-49cc-a1ec-697fb0a00fdd)
+![Screenshot 2024-07-19 183953](https://github.com/user-attachments/assets/4533d378-0b03-48dc-a4eb-4542884ee870)
+
+
+![Screenshot 2024-07-19 183725](https://github.com/user-attachments/assets/058924bd-eb77-4219-b1e7-a349b14f6934)
---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,124 @@
+import React, { useContext, useState } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+const Cart = () => {
+
+  const {cartItem, art_list, removeFromCart , getTotalCartAmount ,url} = useContext(StoreContext);
+  const navigate = useNavigate();
+  const [finalCost , setFinalCost] = useState(2);
+  const [showText , setShowText] = useState(false);
+  const [enter , setEnter] = useState(false);
+  const [success,setSuccess] = useState("");
+
+  const [data , setData] = useState({promocode:""});
+
+  const changeHandler = (e) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+    
+    setData((prev)=>({
+      ...prev,
+      [name]:value
+    }))
+    console.log(data.promocode);
+  }
+
+  const checkValid = () =>{
+    if(data.promocode === 'NAMAN' || data.promocode === 'NEERAJ'){
+      setFinalCost(finalCost-5);
+      data.promocode = "";
+      setShowText(false);
+      setEnter(true);
+      setSuccess("SUCCESS !");
+    }
+    else{
+      setShowText(true);
+    }
+  }
+
+  return (
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br/>
+        <hr />
+        {
+          art_list.map((item,index) =>{
+            if(cartItem[item._id]>0){
+              {
+                return (
+                  <div>
+                      <div className="cart-items-title cart-items-item">
+                        <img src={url+"/images/"+item.image} alt="" />
+                        <p>{item.name}</p>
+                        <p>${item.price}</p>
+                        
+                        <p>{cartItem[item._id]}</p>
+                        <p>${item.price * cartItem[item._id]}</p>
+                        <p onClick={()=>removeFromCart(item._id)} className='cross'>X</p>
+                      </div>
+
+                      <hr />
+
+                  </div>
+                  
+                )
+              }
+            }
+          })
+        }
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div >
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr/>
+
+            <div className="cart-total-details">
+              <p>Delivery Fees</p>
+              <p>${getTotalCartAmount() == 0 ? 0 :2}</p>
+            </div>
+            <hr/>
+
+            <div className="cart-total-details">
+              <b>Total</b>
+              <p>${getTotalCartAmount() == 0 ? 0 : Math.max(0,getTotalCartAmount()+finalCost)}</p>
+            </div>
+          </div>
+
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+
+        </div>
+        
+        <div className='cart-promocode'>
+            <div>
+              <p>If you have a promo code , enter it!</p>
+              <div className='cart-promocode-input'>
+                  <input type="text" placeholder='PROMOCODE' disabled={enter} name='promocode' value={data.promocode} onChange={changeHandler}/>
+                  <button onClick={checkValid}>Submit</button>
+                  
+              </div>
+                  {
+                    showText ? <div className='promo-text'>INVALID PROMO CODE !</div> : <div className='promo-success'>{success}</div>
+                  }
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
